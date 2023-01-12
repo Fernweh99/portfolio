7303d13f79caf04f6029ea34ae460539bcb24cf6
Fernweh99 <daniele.dimento@gmail.com>
2023-01-12 18:18:29 +0100

    work in progress on WorkPage

--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="works">
     <div class="container text-center">
-      <h1 class="display-1">See all my works on GitHub</h1>
+      <h1 class="display-1 text-warning">Work in progress</h1>
+      <i class="fa-solid fa-laptop-code text-danger fa-8x"></i>
+      <h1 class="display-2 mt-5">But you can see my github</h1>
+      <a href="https://github.com/Fernweh99" class="display-3 text-primary">-> Click Here &lt;-</a>
     </div>
   </div>
 </template>
